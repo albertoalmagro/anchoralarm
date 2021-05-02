29da3d768f566ac3ced752768d5264e034014500
Alberto Almagro <albertoalmagro@gmail.com>
2021-05-02 20:09:18 +0200

    Initial Xcode project creation

---
+++ anchoralarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anchoralarm
+//
+//  Created by Alberto Almagro Sotelo on 2/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ anchoralarm/anchoralarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  anchoralarmApp.swift
+//  anchoralarm
+//
+//  Created by Alberto Almagro Sotelo on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct anchoralarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
